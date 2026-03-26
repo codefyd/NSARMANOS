9151b7030a1bb9a8a6dd01e57dc4045296e3050e
codefyd <ahsnsy77@gmail.com>
2026-03-26 18:53:18 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 window.APP_CONFIG = {
-  // استخدم رابط Google Apps Script الصحيح
-  API_URL: 'https://script.google.com/macros/s/AKfycbxSJFkX6Bj8xqP0ByL54JTSGws3OXEhIbiMtCBDLpCUk3m1WRGBoyPYTH8Rr_6Kdi5N/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbzKhGFINGg97MdpQVpAG73sO7LByhWR7epzHQrgvQXKagIOWsMmjSp_OxVOyaHKkpBM/exec'
 };
