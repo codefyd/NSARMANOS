b5c4102f23b180962ef77405d5419b7851dcc31b
codefyd <ahsnsy77@gmail.com>
2026-03-26 17:10:12 +0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// ============================================================================
-// إعدادات التطبيق
-// ============================================================================
-
-window.APP_CONFIG = {
-  // ضع رابط Google Apps Script المنشور هنا
-  // مثال: https://script.google.com/macros/s/AKfycbx...../exec
-  API_URL: 'https://script.google.com/macros/s/AKfycbxSJFkX6Bj8xqP0ByL54JTSGws3OXEhIbiMtCBDLpCUk3m1WRGBoyPYTH8Rr_6Kdi5N/exec'
-};
