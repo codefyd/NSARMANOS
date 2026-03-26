75df7d04857a7f7520bd80a6db2d9a071c2415e7
codefyd <ahsnsy77@gmail.com>
2026-03-26 17:10:44 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// ============================================================================
+// إعدادات التطبيق
+// ============================================================================
+
+window.APP_CONFIG = {
+  // رابط Google Apps Script المنشور
+  API_URL: 'https://script.google.com/macros/s/AKfycbxSJFkX6Bj8xqP0ByL54JTSGws3OXEhIbiMtCBDLpCUk3m1WRGBoyPYTH8Rr_6Kdi5N/exec'
+};
